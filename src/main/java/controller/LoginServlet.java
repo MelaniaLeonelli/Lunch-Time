dda9d10248c86961f3ecd5e66b33b95f7bce3d0a
Melania Leonelli <melaniamlml98@gmail.com>
2023-01-08 23:35:15 +0100

    librerie

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -10,7 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.InfoUtente;
 import model.Utente;
 import model.UtenteDAO;
 
--- src/main/java/model/PreferitiDAO.java
+++ src/main/java/model/PreferitiDAO.java
@@ -1,4 +1,12 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 import controller.DriverManagerConnectionPool;
 
